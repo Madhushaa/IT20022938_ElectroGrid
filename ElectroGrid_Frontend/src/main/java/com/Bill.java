7fc9ccbea70a5cb7e63249c8d21051924423b486
Madhushaa <madushahanshini1@gmailcom>
2022-05-11 22:19:39 +0530

    Implement Database Connection

---
+++ ElectroGrid_Frontend/src/main/java/com/Bill.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Bill {
+	
+	//A common method to connect to the DB
+	private Connection connect(){ 
+		
+		Connection con = null; 
+		
+		try{ 
+				Class.forName("com.mysql.cj.jdbc.Driver"); 
+
+				//Provide the correct details: DBServer/DBName, username, password 
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid_bill", "root", ""); 
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			} 
+		
+		return con; 
+}
+
+}
